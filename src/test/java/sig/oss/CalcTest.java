af8b0341af8966da029f4b6ad97edec8683cb442
Jaehun, Bang <jhb@oslab.khu.ac.kr>
2016-08-24 13:30:34 +0900

    add sub function and test code on Calc and CalcTest java

--- src/test/java/sig/oss/CalcTest.java
+++ src/test/java/sig/oss/CalcTest.java
@@ -1,9 +1,13 @@
 public static org.junit.Assert.*;
 import org.junit.Test;
 public class CalcTest{
-@Test
-public void testAdd(){
-Calc c = new Calc();
-assertEquals(30, c.add(10,20));
-}
+	@Test
+	public void testAdd(){
+		Calc c = new Calc();
+		assertEquals(30, c.add(10,20));
+	}
+	@Test
+	public void testSub(){
+		assertEquals(10, c.sub(20,10));
+	}
 }
